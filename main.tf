e4a515b6113a387831cf902014323c3ad9a27d64
alex-cloudnets <154507173+alex-cloudnets@users.noreply.github.com>
2025-03-13 11:38:25 +0100

    Website

--- main.tf
+++ main.tf
@@ -30,18 +30,6 @@ subnets = [
  ]
 }
 
-resource "google_compute_network" "app" {
-  name                    = var.network_name
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "app" {
-  name          = var.network_name
-  ip_cidr_range = var.network_ip_range
-  region        = var.region
-  network       = google_compute_network.app.id
-}
-
 data "google_compute_image" "ubuntu" {
   most_recent = true
   project     = var.image_project
@@ -52,7 +40,8 @@ resource "google_compute_instance" "blog" {
   name         = var.app_name
   machine_type = var.machine_type
 
-  
+  tags = ["$(var.network_name)-web"]
+
   boot_disk {
     initialize_params {
       image = data.google_compute_image.ubuntu.self_link
@@ -65,5 +54,7 @@ resource "google_compute_instance" "blog" {
     }
   }  
 
+
+metadate_startup_script = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/wwww/html/index.html"
   allow_stopping_for_update = true
 }
